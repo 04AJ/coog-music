75d91fc0d74eba8f89cfa693c514b0e87f9dfe2f
Cruz Salas <cruz.danielss@icloud.com>
2023-10-19 10:14:31 -0500

    changed page.tsx text

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import Image from 'next/image'
-import mysql from "mysql2"
+import Image from "next/image";
+import mysql from "mysql2";
 
 const dbconnection = mysql.createConnection({
   host: process.env.MYSQL_HOST,
@@ -9,8 +9,6 @@ const dbconnection = mysql.createConnection({
 });
 
 export default function Home() {
-
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
@@ -25,7 +23,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
+            By{" "}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
@@ -39,12 +37,8 @@ export default function Home() {
       </div>
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        aaaaa
+        hello world
       </div>
-
-
-
-
     </main>
-  )
+  );
 }
