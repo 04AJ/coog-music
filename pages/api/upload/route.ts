fe61cfc94d65809e263b4e84aee14dc239d2af20
Cruz Salas <cruz.danielss@icloud.com>
2023-10-26 16:54:54 -0500

    page folder deletion

--- pages/api/upload/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { PrismaClient } from '@prisma/client';
-
-export async function POST(req: Request) {
-    const prisma = new PrismaClient();
-    const result = await prisma.$executeRaw`INSERT INTO ethnicity (ethnicity_name) VALUES ('african american')`;
-    console.log(result);
-    return new Response(JSON.stringify(result))
-}
