c4a2155b4f43bfa9d4a4403f419ee60cbe7319b6
Cruz Salas <cruz.danielss@icloud.com>
2023-10-27 20:35:40 -0500

    db.ts added createUser WIP

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,14 @@
-
-import Image from 'next/image'
-import mysql, { ConnectionOptions, RowDataPacket } from 'mysql2';
-import Link from 'next/link';
-import useUploadTrackModal from '@/hooks/useUploadTrackModal';
-import Library from '@/components/Library';
+import Image from "next/image";
+import mysql, { ConnectionOptions, RowDataPacket } from "mysql2";
+import Link from "next/link";
+import useUploadTrackModal from "@/hooks/useUploadTrackModal";
+import Library from "@/components/Library";
 import { toast, Toaster } from "react-hot-toast";
-import UploadTrackButton from '@/components/UploadTrackButton';
-import Header from '@/components/Header';
-import { useEffect, useState } from 'react';
-import { Track } from '@/types';
-import getTracks from '@/db'
+import UploadTrackButton from "@/components/UploadTrackButton";
+import Header from "@/components/Header";
+import { useEffect, useState } from "react";
+import { Track } from "@/types";
+import { createUser, getTracks } from "@/db";
 
 //this means page will not be cached
 export const revalidate = 0;
@@ -17,8 +16,8 @@ export const revalidate = 0;
 let count = 0;
 
 export default async function Home() {
-
   const tracks = await getTracks();
+  //const newuser = await createUser();
   // const uploadModal = useUploadTrackModal();
   // useEffect(() => {
   //   const apiCall = async () => {
@@ -29,19 +28,15 @@ export default async function Home() {
 
   // })
 
-
   return (
     <div className="bg-neutral-900/80 flex min-h-screen flex-col before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-red-500 before:dark:opacity-10 after:dark:from-red-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-      <div><Toaster /></div>
+      <div>
+        <Toaster />
+      </div>
 
-
-      <Header
-        title="Coog Music Library" description="Welcome back"
-      >
-      </Header>
+      <Header title="Coog Music Library" description="Welcome back"></Header>
 
       <UploadTrackButton />
-
       {/* <button className="border p-2 hover:bg-red-700"
         onClick={makeApiCall}>GET Request</button>
 
@@ -52,18 +47,16 @@ export default async function Home() {
       <div className='border p-2'>
         <Link href='/upload-audio-btn'>Upload Audio</Link>
       </div> */}
-
-      <div className='pl-5'> Complete List of Tracks</div>
-      <div className='pl-5'>
-        {tracks.map((track) =>
-          <div key={count++} >
+      <div className="pl-5"> Complete List of Tracks</div>
+      <div className="pl-5">
+        {tracks.map((track) => (
+          <div key={count++}>
             {track.track_name}
             <img src={track.track_img_path} alt="" width="100vw" />
             <audio controls src={track.track_path} />
-          </div>)}
+          </div>
+        ))}
       </div>
-
-
-    </div >
-  )
+    </div>
+  );
 }
--- db.ts
+++ db.ts
@@ -1,17 +1,22 @@
 import { PrismaClient } from "@prisma/client";
+import { Track } from "@/types";
 import { cookies } from "next/headers";
 
-import { Track } from "@/types";
+const prisma = new PrismaClient();
 
-const getTracks = async (): Promise<Track[]> => {
-    const prisma = new PrismaClient();
-    const tracks = await prisma.$queryRaw`SELECT track_name, track_path, track_img_path, artist_id FROM track`
-    // console.log(tracks);
-    // return new Response(JSON.stringify(tracks))
+export async function createUser() {
+  const newUser = await prisma.$executeRaw`
+    INSERT INTO user (user_name,password,birth_date,join_date,email,ethnicity_id,gender_id)
+    VALUES ('myusername','mypassword','2001-01-02','2001-01-02','cruz.danielss@icloud.com',1,1)
+    `;
+  return (newUser as any) || [];
+}
 
+export const getTracks = async (): Promise<Track[]> => {
+  const tracks =
+    await prisma.$queryRaw`SELECT track_name, track_path, track_img_path, artist_id FROM track`;
+  // console.log(tracks);
+  // return new Response(JSON.stringify(tracks))
 
-
-    return (tracks as any) || [];
+  return (tracks as any) || [];
 };
-
-export default getTracks;
