b546858ae944fd78551cc97fb1e66abf8aa7b0a3
Alan Johnson <alanjcd2004@gmail.com>
2023-11-02 18:25:36 -0500

    removed manual login

--- app/(pages)/profile/page.tsx
+++ app/(pages)/profile/page.tsx
@@ -53,21 +53,14 @@ export default function ProfilePage() {
 
                 <Header title="Profile" description="User Details"></Header>
 
-                <div className="flex gap-5 p-4">
-                    <button className="border" onClick={() => { user.setUserId(4); user.setListenerId(3); user.setUserRole("listener") }
-                    }>Login as Listener</button>
-                    <button className="border" onClick={() => { user.setUserId(2); user.setArtistId(2); user.setUserRole('artist') }
-                    }>Login as Artist</button>
-                </div>
-
-                <div>UserID: {user.userId} | UserRole: {user.userRole}</div>
-                {(user.userRole === 'listener') ? <p>ListenerID: {user.listenerId}</p> : <p>ArtistID: {user.artistId}</p>}
 
 
-                {userDetails ? <UserDetails userDetails={userDetails[0]} profilePage={true} /> : null}
+
 
 
             </div >
+            {userDetails ? <UserDetails userDetails={userDetails[0]} profilePage={true} /> : null}
+
 
         </div >
     )
