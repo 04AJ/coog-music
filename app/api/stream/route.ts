a3a6fee4315e64d5b43327a831f796dfa8516f7a
Alan Johnson <alanjcd2004@gmail.com>
2023-11-07 21:38:01 -0600

    added streams api

--- app/api/stream/route.ts
+++ app/api/stream/route.ts
@@ -9,7 +9,19 @@ export async function PATCH(req: NextRequest) {
     const searchParams = req.nextUrl.searchParams;
     const track_id = searchParams.get('track_id');
     const affected = await prisma.$executeRaw`
-    UPDATE TRACK SET streams = streams + 1 WHERE track_id = ${track_id}`
+    UPDATE TRACK SET streams = streams + 1 WHERE track_id = ${track_id}`;
+
 
     return new Response(JSON.stringify(affected));
 }
+
+export async function GET(req: NextRequest) {
+    const searchParams = req.nextUrl.searchParams;
+    const artist_id = searchParams.get('artist_id');
+    const track: [{}] = await prisma.$queryRaw`SELECT SUM(streams) as streams FROM track WHERE artist_id = ${artist_id}`
+
+    return new Response(JSON.stringify(track[0]));
+
+
+
+};
