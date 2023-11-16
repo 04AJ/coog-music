7c4b0d3fcbe0c022881904765345af3bc57c46ed
Cruz Salas <cruz.danielss@icloud.com>
2023-11-16 13:55:54 -0600

    fix targ notif query

--- app/api/notifications/route.ts
+++ app/api/notifications/route.ts
@@ -10,7 +10,7 @@ export async function GET(req: NextRequest) {
     const notifications = await prisma.$queryRaw<Notification[]>`
     SELECT n.*
     FROM notifications n
-    LEFT JOIN listener_follows_artist f ON n.artist_id = f.artist_id
+    LEFT JOIN listener_follows_artists f ON n.artist_id = f.artist_id
     WHERE n.sendAll = 1 OR (n.sendAll = 0 AND f.listener_id = ${listener_id});
     `
 
