e1cedd1662dd37b5da761e3ec2c6ccab63f8ab02
Cruz Salas <cruz.danielss@icloud.com>
2023-11-16 15:53:38 -0600

    added archive to albums and tracks

--- app/api/createdTracks/route.ts
+++ app/api/createdTracks/route.ts
@@ -11,7 +11,7 @@ export async function GET(req: NextRequest) {
     const tracks = await prisma.$queryRaw<Track[]>`
     SELECT track.track_id, track_name, track_path, track_img_path, artist.artist_id, artist_name 
     FROM track, artist 
-    WHERE track.artist_id = artist.artist_id AND track.artist_id = ${artist_id};`
+    WHERE track.artist_id = artist.artist_id AND track.artist_id = ${artist_id} AND track.archive != 1;`
     // console.log(tracks);
     return new Response(JSON.stringify(tracks))
 
