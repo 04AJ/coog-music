fd3238302e8e669584d9553ad742941640f9b31d
Cruz Salas <cruz.danielss@icloud.com>
2023-11-18 16:18:14 -0600

    fix playlist bug

--- app/api/playlistTracks/route.ts
+++ app/api/playlistTracks/route.ts
@@ -9,9 +9,12 @@ export async function GET(req: NextRequest) {
     const searchParams = req.nextUrl.searchParams;
     const playlist_id = searchParams.get('playlist_id');
     const tracks = await prisma.$queryRaw<Track[]>`
-    SELECT track.track_id, track.track_name, track.track_path, track.track_img_path, track.genre_id, artist.artist_id, artist_name 
-    FROM track_to_playlist, track, artist 
-    WHERE track_to_playlist.playlist_id = ${playlist_id} AND track.artist_id = artist.user_id AND track.track_id = track_to_playlist.track_id`
+    SELECT track.track_id, track.track_name, track.track_path, track.track_img_path, track.genre_id, artist.artist_id, artist.artist_name 
+    FROM track_to_playlist
+    INNER JOIN track ON track.track_id = track_to_playlist.track_id
+    INNER JOIN artist ON track.artist_id = artist.artist_id
+    WHERE track_to_playlist.playlist_id = 5;
+    `
     // console.log(tracks);
     return new Response(JSON.stringify(tracks))
 
