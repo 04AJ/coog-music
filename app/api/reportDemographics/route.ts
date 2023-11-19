816e7e3afa078090d93bc5418ef2f13194230387
ka-rl <lykarld@gmail.com>
2023-11-19 14:08:21 -0600

    query

--- app/api/reportDemographics/route.ts
+++ app/api/reportDemographics/route.ts
@@ -24,3 +24,26 @@ export async function GET(req: NextRequest) {
     ));
     return new Response(JSON.stringify(result));
 };
+
+
+
+
+//      this will list the number of liked tracks for each genre
+// select genre_name, count(liked_tracks.track_id) as num_liked
+// from liked_tracks, track, genre
+// where track.genre_id=genre.genre_id and liked_tracks.track_id = track.track_id
+// group by genre_name;
+
+//      this finds the preferred genre of user with id 7
+// select t.genre_id, count(genre_name) as count
+// from liked_tracks as lt, track as t, genre as g, user as u
+// where lt.track_id = t.track_id and t.genre_id = g.genre_id and lt.user_id = u.user_id and u.user_id = 7
+// group by genre_name
+// limit 1;
+
+//     this finds data for people born on 2001, it sees what how many songs of each genre they have liked
+// select t.genre_id, genre_name , count(genre_name) as count
+// from liked_tracks as lt, track as t, genre as g, user as u
+// where lt.track_id = t.track_id and t.genre_id = g.genre_id and lt.user_id = u.user_id and YEAR(u.birth_date) = 2001
+// group by t.genre_id
+// order by count desc;
