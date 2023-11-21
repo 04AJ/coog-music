c0d89ddeab69a76a5e1b757ea24a39b5874096a6
ka-rl <lykarld@gmail.com>
2023-11-21 10:37:46 -0600

    forgor to add these

--- app/api/reportAge/route.ts
+++ app/api/reportAge/route.ts
@@ -9,7 +9,22 @@ export async function GET(req: NextRequest) {
     const searchParams = req.nextUrl.searchParams;
     const artist_id = searchParams.get('artist_id');
     const query = `${artist_id}`;
-    const gender = await prisma.$queryRaw<User[]>`;`;
+    const gender = await prisma.$queryRaw<User[]>`
+
+        SELECT
+            '%' as '',
+            SUM(IF(age BETWEEN 0 and 15,1,0))/COUNT(*) * 100 as '1',
+            SUM(IF(age BETWEEN 16 and 25,1,0))/COUNT(*) * 100 as '2',
+            SUM(IF(age BETWEEN 26 and 35,1,0))/COUNT(*) * 100 '3',
+            SUM(IF(age BETWEEN 36 and 45,1,0))/COUNT(*) * 100 as '4',
+            SUM(IF(age >=46, 1, 0))/COUNT(*) * 100 as '5',
+            SUM(IF(age IS NULL, 1, 0))/COUNT(*)*100 as '6'
+        
+        FROM (
+            SELECT TIMESTAMPDIFF(YEAR, birth_date, CURDATE()) AS age 
+            FROM user, listener_follows_artists as lfa, listener as l, artist as a
+            WHERE lfa.listener_id = l.listener_id AND l.user_id = user.user_id AND lfa.artist_id = a.artist_id AND a.artist_email = ${query}
+            ) as derived;`;
     const result = JSON.parse(JSON.stringify(gender, (key, value) =>
         typeof value === 'bigint'
             ? value.toString()
